17230dc07722da0f1588854fcbfcb26264118a1b
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2011-11-10 21:14:57 +0100

    New specified
    The new method is specified for the Markov class.

---
+++ markov.rb
@@ -0,0 +1,3 @@
+class Markov
+
+end
--- spec/markov_spec.rb
+++ spec/markov_spec.rb
@@ -1,1 +1,10 @@
 require 'markov'
+
+describe "Markov" do
+	describe "#new" do
+		it "creates an instance of the Markov class" do
+			book = Markov.new
+			book.should be_an_instance_of Markov
+		end
+	end
+end
